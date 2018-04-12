a9e05cb00ffc176ea73a9e5784c4efdc420ecb21
Nick Riehl <rhontos@gmail.com>
2018-04-11 18:25:55 -0700

    Mockup created, mostly static

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='grid'>
+        <div id='video' />
+        <div id='buttons'>
+          <img src='person.png' class='top-button hoverable' />
+          <img src='microphone.png' class='top-button hoverable' />
+          <img src='video.png' class='top-button hoverable' />
+          <img src='signal.png' class='top-button hoverable' />
+          <img src='settings.png' class='top-button hoverable' />
+          <img src='phone.png' class='top-button hoverable' />
+        </div>
+        <div id='sidebar'>
+          <img src='chat.png' class='side-button hoverable' />
+          <img src='share.png' class='side-button hoverable' />
+          <img src='camera.png' class='side-button hoverable' />
+          <img src='lifesaver.png' class='side-button hoverable' />
+          <img src='options.png' class='side-button hoverable' />
+        </div>
+        <div id='callers'>
+          <img src='caller.png' class='caller hoverable' />
+          <img src='caller.png' class='caller hoverable' />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
